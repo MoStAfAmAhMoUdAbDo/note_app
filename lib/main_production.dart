776b7657a6b860c3864ce7888b2f3873bc1e292e
MoStAfAmAhMoUdAbDo <mosmahabd952@gmail.com>
2024-04-04 02:54:28 +0200

    -making flavor for the app

---
+++ lib/main_production.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'note_app.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
---
+++ lib/note_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:note_app_new/core/helper/extenions.dart';
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Note App',
+    );
+  }
+}
